0f626d503babe6d3de417effed524915ddd26691
philip-730 <philip.amendolia@gmail.com>
2026-03-30 02:11:13 +0000

    feat(starship): apply catppuccin mocha module styles

--- users/phil-personal.nix
+++ users/phil-personal.nix
@@ -36,6 +36,16 @@
     starship.settings = {
       gcloud.disabled = true;
       username.show_always = true;
+      character = {
+        success_symbol = "[[󰄛](green) ❯](peach)";
+        error_symbol = "[[󰄛](red) ❯](peach)";
+        vimcmd_symbol = "[󰄛 ❮](subtext1)";
+      };
+      git_branch.style = "bold mauve";
+      directory = {
+        truncation_length = 4;
+        style = "bold lavender";
+      };
     };
   };
 
