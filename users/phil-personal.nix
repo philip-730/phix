8347eaff9dae362e84e7176f5fcd665b76a022c1
philip-730 <philip.amendolia@gmail.com>
2026-03-30 06:31:19 +0000

    fix(wezterm): gate WSL domain config behind windows target check

--- users/phil-personal.nix
+++ users/phil-personal.nix
@@ -63,14 +63,16 @@
     opacity = 0.8;
     stableConfigPath = "/home/philip/.config/wezterm-stable.lua";
     extraConfig = ''
-      config.wsl_domains = {
-        {
-          name = "WSL:NixOS",
-          distribution = "NixOS",
-          default_cwd = "/home/philip",
-        },
-      }
-      config.default_domain = "WSL:NixOS"
+      if string.find(wezterm.target_triple, "windows") then
+        config.wsl_domains = {
+          {
+            name = "WSL:NixOS",
+            distribution = "NixOS",
+            default_cwd = "/home/philip",
+          },
+        }
+        config.default_domain = "WSL:NixOS"
+      end
     '';
   };
 }
